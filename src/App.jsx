402efc50ac7a7a6d17c86fdf9195edeeecb9d6d0
Wellington Santos <was.sls@hotmail.com>
2026-04-10 20:57:25 -0300

    Add data.js with core concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
 
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
